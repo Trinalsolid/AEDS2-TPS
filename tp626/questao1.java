32164fcf08ff85e043b8553010388219911be861
Luis Fernando <81189394+Trinalsolid@users.noreply.github.com>
2023-11-20 19:51:27 -0300

    Add files via upload

---
+++ tp626/questao1.java
@@ -0,0 +1,535 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+class Jogador{
+
+    public static Jogador[] jogad = new Jogador[1000];
+    public static int tamJog = 0;
+
+    private int id;
+    private String nome;
+    private int altura;
+    private int peso;
+    private String universidade;
+    private int anoNascimento;
+    private String cidadeNascimento;
+    private String estadoNascimento;
+
+    //CONSTRUTORES
+
+    public Jogador(){
+        this.id = 0;
+        this.nome= "";
+        this.altura= 0;
+        this.peso= 0;
+        this.universidade= "";
+        this.anoNascimento= 0;
+        this.cidadeNascimento= "";
+        this.estadoNascimento= "";
+    }
+
+    public Jogador(int id,String nome,int altura,int peso,
+    String universidade,int anoNascimento,String cidadeNascimento,String estadoNascimento){
+
+        this.id = id;
+        this.nome= nome;
+        this.altura= altura;
+        this.peso= peso;
+        this.universidade= universidade;
+        this.anoNascimento= anoNascimento;
+        this.cidadeNascimento= cidadeNascimento;
+        this.estadoNascimento= estadoNascimento;
+    }
+
+    //GETS
+
+    public int getId() {
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public int getAltura() {
+        return altura;
+    }public int getPeso() {
+        return peso;
+    }
+    public String getUniversidade() {
+        return universidade;
+    }
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
+    public String getCidadeNascimento() {
+        return cidadeNascimento;
+    }
+    public String getEstadoNascimento() {
+        return estadoNascimento;
+    }
+
+    //SETS
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+    public void setUniversidade(String universidade) {
+        this.universidade = universidade;
+    }
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+    public void setCidadeNascimento(String cidadeNascimento) {
+        this.cidadeNascimento = cidadeNascimento;
+    }
+    public void setEstadoNascimento(String estadoNascimento) {
+        this.estadoNascimento = estadoNascimento;
+    }
+
+    //CLONE
+
+    public Jogador clone(){
+
+        Jogador clone = new Jogador();
+
+        clone.id = id;
+        clone.nome = nome;
+        clone.altura = altura;
+        clone.peso = peso;
+        clone.universidade = universidade;
+        clone.anoNascimento = anoNascimento;
+        clone.cidadeNascimento = cidadeNascimento;
+        clone.estadoNascimento = estadoNascimento;
+
+        return clone;
+    }
+
+    //MOSTRAR
+
+    public void mostrar(){
+        System.out.println("["+ getId()+" ## "+ getNome()+" ## "+ getAltura()+" ## "+ getPeso()
+            +" ## "+ getAnoNascimento()+" ## "+ getUniversidade()+" ## "+getCidadeNascimento()+
+            " ## " +getEstadoNascimento()+" ## "+"]");
+    }
+}
+
+class No {
+    public Jogador elemento; // Conteudo do no.
+    public No esq, dir;  // Filhos da esq e dir.
+
+    /**
+     * Construtor da classe.
+     * @param elemento Conteudo do no.
+     */
+    public No(Jogador elemento) {
+        this(elemento, null, null);
+    }
+
+    /**
+     * Construtor da classe.
+     * @param elemento Conteudo do no.
+     * @param esq No da esquerda.
+     * @param dir No da direita.
+     */
+    public No(Jogador elemento, No esq, No dir) {
+        this.elemento = elemento;
+        this.esq = esq;
+        this.dir = dir;
+    }
+}
+
+class ArvoreBinaria {
+	private No raiz; // Raiz da arvore.
+
+	/**
+	 * Construtor da classe.
+	 */
+	public ArvoreBinaria() {
+		raiz = null;
+	}
+
+	/**
+	 * Metodo publico iterativo para pesquisar elemento.
+	 * @param x Elemento que sera procurado.
+	 * @return <code>true</code> se o elemento existir,
+	 * <code>false</code> em caso contrario.
+	 */
+	public boolean pesquisar(String x) {
+        System.out.print("raiz ");
+		return pesquisar(x, raiz);
+	}
+
+	/**
+	 * Metodo privado recursivo para pesquisar elemento.
+	 * @param x Elemento que sera procurado.
+	 * @param i No em analise.
+	 * @return <code>true</code> se o elemento existir,
+	 * <code>false</code> em caso contrario.
+	 */
+	private boolean pesquisar(String x, No i) {
+        boolean resp;
+        if (i == null) {
+            questao1.contador++;
+            resp = false;
+        }else if (x.compareTo(i.elemento.getNome()) == 0) {
+            questao1.contador++;
+            resp = true;
+        }else if ( x.compareTo(i.elemento.getNome()) < 0 ) {
+            questao1.contador++;
+            MyIO.print("esq "); 
+            resp = pesquisar(x, i.esq);
+        }else {
+            questao1.contador++;
+            MyIO.print("dir "); 
+            resp = pesquisar(x, i.dir);
+        }
+        return resp;
+	}
+
+	/**
+	 * Metodo publico iterativo para exibir elementos.
+	 */
+	public void caminharCentral() {
+		System.out.print("[ ");
+		caminharCentral(raiz);
+		System.out.println("]");
+	}
+
+	/**
+	 * Metodo privado recursivo para exibir elementos.
+	 * @param i No em analise.
+	 */
+	private void caminharCentral(No i) {
+		if (i != null) {
+			caminharCentral(i.esq); // Elementos da esquerda.
+			System.out.print(i.elemento+ " "); // Conteudo do no.
+			caminharCentral(i.dir); // Elementos da direita.
+		}
+	}
+
+	/**
+	 * Metodo publico iterativo para exibir elementos.
+	 */
+	public void caminharPre() {
+		System.out.print("[ ");
+		caminharPre(raiz);
+		System.out.println("]");
+	}
+
+	/**
+	 * Metodo privado recursivo para exibir elementos.
+	 * @param i No em analise.
+	 */
+	private void caminharPre(No i) {
+		if (i != null) {
+			System.out.print(i.elemento + " "); // Conteudo do no.
+			caminharPre(i.esq); // Elementos da esquerda.
+			caminharPre(i.dir); // Elementos da direita.
+		}
+	}
+
+	/**
+	 * Metodo publico iterativo para exibir elementos.
+	 */
+	public void caminharPos() {
+		System.out.print("[ ");
+		caminharPos(raiz);
+		System.out.println("]");
+	}
+
+	/**
+	 * Metodo privado recursivo para exibir elementos.
+	 * @param i No em analise.
+	 */
+	private void caminharPos(No i) {
+		if (i != null) {
+			caminharPos(i.esq); // Elementos da esquerda.
+			caminharPos(i.dir); // Elementos da direita.
+			System.out.print(i.elemento + " "); // Conteudo do no.
+		}
+	}
+
+
+	/**
+	 * Metodo publico iterativo para inserir elemento.
+	 * @param x Elemento a ser inserido.
+	 * @throws Exception Se o elemento existir.
+	 */
+	public void inserir(Jogador x) throws Exception{
+		raiz = inserir(x, raiz);
+	}
+
+	/**
+	 * Metodo privado recursivo para inserir elemento.
+	 * @param x Elemento a ser inserido.
+	 * @param i No em analise.
+	 * @return No em analise, alterado ou nao.
+	 * @throws Exception Se o elemento existir.
+	 */
+	private No inserir(Jogador x, No i) throws Exception{
+		if (i == null) {
+            i = new No(x);
+        }else if (x.getNome().compareTo(i.elemento.getNome()) < 0) {
+            i.esq = inserir(x, i.esq);
+        }else if (x.getNome().compareTo(i.elemento.getNome()) > 0) {
+            i.dir = inserir(x, i.dir);
+        }else {
+            throw new Exception("Erro ao inserir!");
+        }
+
+        return i;
+	}
+
+	/**
+	 * Metodo publico iterativo para remover elemento.
+	 * @param x Elemento a ser removido.
+	 * @throws Exception Se nao encontrar elemento.
+	 */
+	public void remover(String x) throws Exception {
+		raiz = remover(x, raiz);
+	}
+
+	/**
+	 * Metodo privado recursivo para remover elemento.
+	 * @param x Elemento a ser removido.
+	 * @param i No em analise.
+	 * @return No em analise, alterado ou nao.
+	 * @throws Exception Se nao encontrar elemento.
+	 */
+	private No remover(String x, No i) throws Exception {
+
+		if (i == null) {
+            throw new Exception("Erro ao remover!");
+        } else if ( x.compareTo(i.elemento.getNome()) < 0) {
+            i.esq = remover(x, i.esq);
+        } else if ( x.compareTo(i.elemento.getNome()) > 0) {
+            i.dir = remover(x, i.dir);
+        // Sem no a direita.
+        } else if (i.dir == null) {
+            i = i.esq;
+        // Sem no a esquerda.
+        } else if (i.esq == null) {
+            i = i.dir;
+        // No a esquerda e no a direita.
+        } else {
+            i.esq = maiorEsq(i, i.esq);
+        }
+
+		return i;
+	}
+
+	/**
+	 * Metodo para trocar o elemento "removido" pelo maior da esquerda.
+	 * @param i No que teve o elemento removido.
+	 * @param j No da subarvore esquerda.
+	 * @return No em analise, alterado ou nao.
+	 */
+	private No maiorEsq(No i, No j) {
+      // Encontrou o maximo da subarvore esquerda.
+		if (j.dir == null) {
+			i.elemento = j.elemento; // Substitui i por j.
+			j = j.esq; // Substitui j por j.ESQ.
+            // Existe no a direita.
+		} else {
+         // Caminha para direita.
+			j.dir = maiorEsq(i, j.dir);
+		}
+		return j;
+	}
+
+	/**
+	 * Metodo que retorna a altura da árvore
+	 * @return int altura da árvore
+	 */
+    public int getAltura(){
+        return getAltura(raiz, 0);
+    }
+
+	/**
+	 * Metodo que retorna a altura da árvore
+	 * @return int altura da árvore
+	 */
+   public int getAltura(No i, int altura){
+      if(i == null){
+         altura--;
+      } else {
+         int alturaEsq = getAltura(i.esq, altura + 1);
+         int alturaDir = getAltura(i.dir, altura + 1);
+         altura = (alturaEsq > alturaDir) ? alturaEsq : alturaDir;
+      }
+      return altura;
+   }
+
+
+	/**
+	 * Metodo publico iterativo para remover elemento.
+	 * @param x Elemento a ser removido.
+	 * @throws Exception Se nao encontrar elemento.
+	 */
+	public void remover2(Jogador x) throws Exception {
+        if (raiz == null) {
+            throw new Exception("Erro ao remover2!");
+        } else if(x.getNome().compareTo(raiz.elemento.getNome()) < 0){
+            remover2(x, raiz.esq, raiz);
+        } else if (x.getNome().compareTo(raiz.elemento.getNome()) > 0){
+            remover2(x, raiz.dir, raiz);
+        } else if (raiz.dir == null) {
+            raiz = raiz.esq;
+        } else if (raiz.esq == null) {
+            raiz = raiz.dir;
+        } else {
+            raiz.esq = maiorEsq(raiz, raiz.esq);
+        }
+    }
+
+	/**
+	 * Metodo privado recursivo para remover elemento.
+	 * @param x Elemento a ser removido.
+	 * @param i No em analise.
+	 * @param pai do No em analise.
+	 * @throws Exception Se nao encontrar elemento.
+	 */
+	private void remover2(Jogador x, No i, No pai) throws Exception {
+		if (i == null) {
+            throw new Exception("Erro ao remover2!");
+        } else if (x.getNome().compareTo(i.elemento.getNome()) < 0) {
+            remover2(x, i.esq, i);
+        } else if (x.getNome().compareTo(i.elemento.getNome()) > 0) {
+            remover2(x, i.dir, i);
+        } else if (i.dir == null) {
+            pai = i.esq;
+        } else if (i.esq == null) {
+            pai = i.dir;
+        } else {
+            i.esq = maiorEsq(i, i.esq);
+		}
+	}
+
+    public Jogador getRaiz() throws Exception {
+        return raiz.elemento;
+    }
+
+    public static boolean igual (ArvoreBinaria a1, ArvoreBinaria a2){
+        return igual(a1.raiz, a2.raiz);
+    }
+
+    private static boolean igual (No i1, No i2){
+        boolean resp;
+        if(i1 != null && i2 != null){
+            resp = (i1.elemento == i2.elemento) && igual(i1.esq, i2.esq) && igual(i1.dir, i2.dir);
+        } else if(i1 == null && i2 == null){
+            resp = true;
+        } else {
+            resp = false; 
+        }
+        return resp;
+    }
+}
+
+public class questao1{
+    public static Jogador[] jogad = new Jogador[1000];
+    public static int tamJog = 0;
+    public static int contador = 0;
+    
+    //LER
+    public static String ler(String entradaid) throws Exception {
+        String entrada = "";
+        BufferedReader arq = new BufferedReader(new InputStreamReader(new FileInputStream("/tmp/players.csv")));
+        entrada = arq.readLine();
+        while(entrada != null){
+            String[] Ids = entrada.split(",");
+            if(Ids[0].equals(entradaid) == true){
+                return entrada;
+            }
+            entrada = arq.readLine();
+        }
+        String saida = "";
+        
+        return saida;
+    }
+
+    public static void TratarString(String entrada)throws Exception{
+        int tam = entrada.length();
+        if(entrada.charAt(tam-1) == ','){
+            entrada = entrada + "nao informado";
+        }
+        entrada = entrada.replaceAll(",,", ",nao informado,");
+        String[] jogadores = entrada.split(",");
+
+        // ID do jogador
+        int id = Integer.parseInt(jogadores[0]);
+        jogad[tamJog].setId(id);
+        // Nome do jogador
+        jogad[tamJog].setNome(jogadores[1]);
+        // Altura do jogador
+        int altura = Integer.parseInt(jogadores[2]);
+        jogad[tamJog].setAltura(altura);
+        // Peso do jogador
+        int peso = Integer.parseInt(jogadores[3]);
+        jogad[tamJog].setPeso(peso);
+        // Universidade do jogador
+        jogad[tamJog].setUniversidade(jogadores[4]);
+        // Ano de nascimento do jogador
+        int anoNascimento = Integer.parseInt(jogadores[5]);
+        jogad[tamJog].setAnoNascimento(anoNascimento);
+        // Cidade do  jogador
+        jogad[tamJog].setCidadeNascimento(jogadores[6]);
+        // Estado do jogador
+        jogad[tamJog].setEstadoNascimento(jogadores[7]);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        //tempo inicial do código
+        long tempoInicial = System.currentTimeMillis();
+        //----------------------------------------------//
+        
+        // PRIMEIRA PARTE DA LEITURA
+        ArvoreBinaria arvoreB = new ArvoreBinaria();
+        String IdsJogadores = "";
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+        IdsJogadores = entrada.readLine();
+
+        while(IdsJogadores.equals("FIM") != true){
+            jogad[tamJog] = new Jogador();
+            TratarString(ler(IdsJogadores));
+            tamJog ++;
+            IdsJogadores = entrada.readLine();
+        }
+        for(int i =0 ; i < tamJog ; i++){
+            arvoreB.inserir(jogad[i]);
+        }
+        
+        // SEGUNDA PARTE DA LEITURA
+        String NomeJogadores = "";
+        boolean saida = false;
+        BufferedReader entrada2 = new BufferedReader(new InputStreamReader(System.in));
+        NomeJogadores = entrada2.readLine();
+
+        while(NomeJogadores.equals("FIM") != true){
+            System.out.print(NomeJogadores + " ");
+            saida = arvoreB.pesquisar(NomeJogadores);
+            if(saida == false){
+                System.out.println("NAO");
+            }else{
+                System.out.println("SIM"); 
+            }
+            NomeJogadores = entrada2.readLine();
+        }
+
+        //arquivo de Matricula sequencial 
+        long tempoFinal = System.currentTimeMillis();
+        Arq.openWrite("matrícula_arvoreBinaria.txt");
+        Arq.println("695161" + "\t" + (tempoFinal - tempoInicial) + "\t" + contador);
+        Arq.close();
+    } 
+}
